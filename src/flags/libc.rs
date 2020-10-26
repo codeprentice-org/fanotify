c136c17a2846cc3d0642f313f1a2672af5d5ff49
Khyber Sen <kkysen@gmail.com>
2020-10-26 19:22:39 -0400

    Separated flags in libc.rs into commented sections, but I should also add full documentation.

--- src/flags/libc.rs
+++ src/flags/libc.rs
@@ -3,6 +3,38 @@
 // TODO add documentation
 // TODO check that all the types are correct
 
+// Flags
+pub const FAN_CLOEXEC: u32 = 0x00000001;
+pub const FAN_NONBLOCK: u32 = 0x00000002;
+pub const FAN_UNLIMITED_QUEUE: u32 = 0x00000010;
+pub const FAN_UNLIMITED_MARKS: u32 = 0x00000020;
+pub const FAN_REPORT_TID: u32 = 0x00000100;
+pub const FAN_REPORT_FID: u32 = 0x00000200;
+pub const FAN_REPORT_DIR_FID: u32 = 0x00000400;
+pub const FAN_REPORT_NAME: u32 = 0x00000800;
+
+// NotificationClass < Flags
+pub const FAN_CLASS_NOTIF: u32 = 0x00000000;
+pub const FAN_CLASS_CONTENT: u32 = 0x00000004;
+pub const FAN_CLASS_PRE_CONTENT: u32 = 0x00000008;
+
+// MarkAction < CombinedMarkFlags
+pub const FAN_MARK_ADD: u32 = 0x00000001;
+pub const FAN_MARK_REMOVE: u32 = 0x00000002;
+pub const FAN_MARK_FLUSH: u32 = 0x00000080;
+
+// MarkWhat < CombinedMarkFlags
+pub const FAN_MARK_INODE: u32 = 0x00000000;
+pub const FAN_MARK_MOUNT: u32 = 0x00000010;
+pub const FAN_MARK_FILESYSTEM: u32 = 0x00000100;
+
+// MarkFlags < CombinedMarkFlags
+pub const FAN_MARK_DONT_FOLLOW: u32 = 0x00000004;
+pub const FAN_MARK_ONLYDIR: u32 = 0x00000008;
+pub const FAN_MARK_IGNORED_MASK: u32 = 0x00000020;
+pub const FAN_MARK_IGNORED_SURV_MODIFY: u32 = 0x00000040;
+
+// MarkMask
 pub const FAN_ACCESS: u64 = 0x00000001;
 pub const FAN_MODIFY: u64 = 0x00000002;
 pub const FAN_ATTRIB: u64 = 0x00000004;
@@ -22,27 +54,8 @@ pub const FAN_ACCESS_PERM: u64 = 0x00020000;
 pub const FAN_OPEN_EXEC_PERM: u64 = 0x00040000;
 pub const FAN_EVENT_ON_CHILD: u64 = 0x08000000;
 pub const FAN_ONDIR: u64 = 0x40000000;
-pub const FAN_CLOEXEC: u32 = 0x00000001;
-pub const FAN_NONBLOCK: u32 = 0x00000002;
-pub const FAN_CLASS_NOTIF: u32 = 0x00000000;
-pub const FAN_CLASS_CONTENT: u32 = 0x00000004;
-pub const FAN_CLASS_PRE_CONTENT: u32 = 0x00000008;
-pub const FAN_UNLIMITED_QUEUE: u32 = 0x00000010;
-pub const FAN_UNLIMITED_MARKS: u32 = 0x00000020;
-pub const FAN_REPORT_TID: u32 = 0x00000100;
-pub const FAN_REPORT_FID: u32 = 0x00000200;
-pub const FAN_REPORT_DIR_FID: u32 = 0x00000400;
-pub const FAN_REPORT_NAME: u32 = 0x00000800;
-pub const FAN_MARK_ADD: u32 = 0x00000001;
-pub const FAN_MARK_REMOVE: u32 = 0x00000002;
-pub const FAN_MARK_DONT_FOLLOW: u32 = 0x00000004;
-pub const FAN_MARK_ONLYDIR: u32 = 0x00000008;
-pub const FAN_MARK_IGNORED_MASK: u32 = 0x00000020;
-pub const FAN_MARK_IGNORED_SURV_MODIFY: u32 = 0x00000040;
-pub const FAN_MARK_FLUSH: u32 = 0x00000080;
-pub const FAN_MARK_INODE: u32 = 0x00000000;
-pub const FAN_MARK_MOUNT: u32 = 0x00000010;
-pub const FAN_MARK_FILESYSTEM: u32 = 0x00000100;
+
+// etc.
 pub const FANOTIFY_METADATA_VERSION: u32 = 3;
 pub const FAN_EVENT_INFO_TYPE_FID: u32 = 1;
 pub const FAN_EVENT_INFO_TYPE_DFID_NAME: u32 = 2;
