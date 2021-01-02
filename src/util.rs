ccf065f1e14786ee5e4e7bf38f8d679d05f458f5
Khyber Sen <kkysen@gmail.com>
2021-01-02 12:23:02 -0500

    Finished and touched up the docs for util.rs.

--- src/util.rs
+++ src/util.rs
@@ -3,9 +3,10 @@ use std::ops::Neg;
 use nix::errno::Errno;
 use thiserror::Error;
 
-/// This macro gives a 0 or 1 trait for every number type
-/// allows you to write libc generically so that it can 
-/// handle multiple integer types, e.g. i32 or u32
+/// Defines 0 or 1 for every number type using a macro.
+///
+/// This allows you to write libc calls generically so that it can
+/// handle multiple integer types, e.g. i32, i64, or u32.
 pub trait ZeroOne {
     const ZERO: Self;
     const ONE: Self;
@@ -21,6 +22,8 @@ macro_rules! impl_zero_one {
 impl_zero_one! { u8 i8 u16 i16 u32 i32 u64 i64 usize isize f32 f64 }
 
 
+/// Make a libc call, detecting -1 return values
+/// and return an [`Err`] with an [`Errno`] in that case.
 pub fn libc_call<T: ZeroOne + Copy + Eq + Neg<Output=T>, F: FnOnce() -> T>(f: F) -> Result<T, Errno> {
     Errno::clear();
     let result = f();
@@ -33,6 +36,7 @@ pub fn libc_call<T: ZeroOne + Copy + Eq + Neg<Output=T>, F: FnOnce() -> T>(f: F)
     }
 }
 
+/// Make a libc call like [`libc_call`], except throw away the return value.
 pub fn libc_void_call<T: ZeroOne + Copy + Eq + Neg<Output=T>, F: FnOnce() -> T>(f: F) -> Result<(), Errno> {
     if libc_call(f)? == T::ZERO {
         Ok(())
@@ -41,6 +45,13 @@ pub fn libc_void_call<T: ZeroOne + Copy + Eq + Neg<Output=T>, F: FnOnce() -> T>(
     }
 }
 
+/// A syscall error that should not occur according to how the syscall is documented.
+/// For example, an [`EINVAL`](Errno::EINVAL) even though all the arguments are indeed valid.
+/// Or an [`Errno`] error that the syscall is not documented as ever returning.
+///
+/// The error struct contains the syscall name,
+/// the arguments to the syscall as a [`String`],
+/// and the unexpected [`Errno`] received.
 #[derive(Error, Debug)]
 #[error("impossible error in syscall {}({}): {:?}", .syscall, .args, .errno)]
 pub struct ImpossibleSysCallError {
